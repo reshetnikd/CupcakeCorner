6deb746425c73d1fa6f10cf0af36b864611c73de
reshetnikd <dmitry.reshetnik@icloud.com>
2020-02-15 13:54:54 +0200

    Try out validating and disabling forms

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -8,72 +8,28 @@
 
 import SwiftUI
 
-class User: ObservableObject, Codable {
-    enum CodingKeys: CodingKey {
-        case name
-    }
-    
-    @Published var name = "Dmitry Reshetnik"
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(name, forKey: .name)
-    }
-    
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        name = try container.decode(String.self, forKey: .name)
-    }
-}
-
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
 struct ContentView: View {
-    @State private var results = [Result]()
+    @State private var username = ""
+    @State private var email = ""
+    
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
+    }
     
     var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
-            }
-        }
-        .onAppear(perform: loadData)
-    }
-    
-    func loadData() {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        
-        let request = URLRequest(url: url)
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if let data = data {
-                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                    // we have good data - go back to the main thread
-                    DispatchQueue.main.async {
-                        // update our UI
-                        self.results = decodedResponse.results
-                    }
-                    
-                    // everything is good, so we can exit
-                    return
-                }
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
             }
             
-            // if we're still here it means there was a problem
-            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
-        }.resume()
+            Section {
+                Button("Create account") {
+                    print("Creating account...")
+                }
+            }
+            .disabled(disableForm)
+        }
     }
 }
 
